62343dc145f88e4e97218e3b0c3326fb6c4b8b3f
arlawrenc <65216386+arlawrenc@users.noreply.github.com>
2020-08-28 22:57:49 -0400

    File Upload 202002
    Corrected File Upload

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -4,33 +4,36 @@ var tableData = data;
 // YOUR CODE HERE!
 function pageLoad()
 {
-    var content="";
-    for (var i in tableData)
-    {
-        var row = `<tr><td class="table-row">${tableData[i].datetime}</td><td class="table-row">${tableData[i].city}</td><td class="table-row"${tableData[i].state}</td><td class="table-row">${tableData[i].country}</td><td class="table-row">${tableData[i].shape}</td><td class="table-row">${tableData[i].durationMinutes}</td><td class="table-row">${tableData[i].comments}</td></tr>`;
-        content += row;        
-    }
-
-    $("#divTableBody").html(content);
+    //alert("pageLoad");
+    toggleDiv("divInputForm");
+    
 }
 
-function addNewRow(toggle)
+function toggleDiv(div) {
+    var x = document.getElementById(div);
+    //alert(x.style.display);
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+function addNewRow(flag)
 {
-    if (toggle===1)
-       $("#divInputForm").show();
-    else
-        $("#divInputForm").hide();
+   // alert("addNewRow");
+    toggleDiv("divInputForm");
 }
 function pushNewRow()
 {
-    var dt = $("#datetime2").val();
-    var city = $("#city2").val();
-    var state = $("#state2").val();
-    var country = $("#country2").val();
-    var shape = $("#shape2").val();
-    var comments= $("#comments").val();
-    var duration= $("#durationMinutes").val();
-
+    var dt = document.getElementById("datetime2").value;
+    var city = document.getElementById("city2").value;
+    var state = document.getElementById("state2").value;
+    var country =document.getElementById("country2").value;
+    var shape = document.getElementById("shape2").value;
+    var comments= document.getElementById("comments").value;
+    var duration= document.getElementById("durationMinutes").value;
+//alert(city);
     data.push ({
         datetime: dt,
         city: city,
@@ -42,20 +45,21 @@ function pushNewRow()
     }
     );
 
-    $("#datetime2").val("");
-    $("#city2").val("");
-    $("#state2").val("");
-    $("#country2").val("");
-    $("#shape2").val("");
-    $("#durationMinutes").val("");       
-    $("#comments").val("");     
+    document.getElementById("datetime2").value="";
+    document.getElementById("city2").value="";
+    document.getElementById("state2").value="";
+    document.getElementById("country2").value="";
+    document.getElementById("shape2").value="";
+    document.getElementById("durationMinutes").value="";
+    document.getElementById("comments").value="";
+   
     alert("row added");
-    $("#divInputForm").hide();
+    toggleDiv("divInputForm");
 }
 function findByDate()
 {
     var dt = document.getElementById("datetime").value;
-    alert(dt);
+  //  alert(dt);
     var content="";
     for (var i in tableData)
     {
@@ -65,8 +69,8 @@ function findByDate()
             content += row;        
         }
     }
-
-    $("#divTableBody").html(content);    
+  
+    document.getElementById("divTableBody").innerHTML=content;    
 }
 /*
 * Using multiple `input` tags and/or select dropdowns, write JavaScript code so the user can to set multiple filters and search for UFO sightings using the following criteria based on the table columns:
@@ -79,32 +83,35 @@ function findByDate()
   */
  function findByMultiple()
  {
-     var dt = $("#datetime").val();
-     var city = $("#city").val().toUpperCase();
-     var state = $("#state").val();
-     var country = $("#country").val().toUpperCase();
-     var shape = $("#shape").val().toUpperCase();
 
-    // alert("findByMultiple"+city);
+    var dt = document.getElementById("datetime").value.toUpperCase();
+    var city = document.getElementById("city").value.toUpperCase();
+    var state = document.getElementById("state").value.toUpperCase();
+    var country =document.getElementById("country").value.toUpperCase();
+    var shape = document.getElementById("shape").value;
+
      var content="";
      for (var i in tableData)
      {
          if (
-             (tableData[i].datetime===dt||dt==="") &&
+             (tableData[i].datetime==dt||dt=="") &&
              (tableData[i].city.toUpperCase()===city||city==="") &&
-             (tableData[i].state.toUpperCase()===state||state==="") &&
-             (tableData[i].country.toUpperCase()===country||country==="") &&
-             (tableData[i].shape.toUpperCase()===shape||shape==="")
+             (tableData[i].state.toUpperCase()==state||state=="") &&
+             (tableData[i].country.toUpperCase()==country||country=="") &&
+             (tableData[i].shape.toUpperCase()==shape||shape=="")
          )
          {
+             //alert("found: "+tableData[i].city);
              var row = `<tr><td class="table-row">${tableData[i].datetime}</td><td class="table-row">${tableData[i].city}</td><td class="table-row">${tableData[i].state}</td><td class="table-row">${tableData[i].country}</td><td class="table-row">${tableData[i].shape}</td><td class="table-row">${tableData[i].durationMinutes}</td><td class="table-row">${tableData[i].comments}</td></tr>`;
              content += row;        
          }
      }
-     $("#datetime").val("");
-     $("#city").val("");
-     $("#state").val("");
-     $("#country").val("");
-     $("#shape").val("");     
-     $("#divTableBody").html(content);    
+     //alert(content);
+     document.getElementById("datetime").value="";
+     document.getElementById("city").value="";
+     document.getElementById("state").value="";
+     document.getElementById("country").value="";
+     document.getElementById("shape").value="";
+     document.getElementById("divTableBody").innerHTML=content; 
+  
  }
